c1291e8d57fe93291eafa0e7af01e3724fcf8f7b
Braydon <xcoolmanxx@gmail.com>
2021-03-16 20:19:09 -0400

    Documented the api/player package
    Took 4 minutes

---
+++ API/src/main/java/me/braydon/api/player/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Contains the violation class which is used internally to track violations.
+ *
+ * @author Braydon
+ * @see me.braydon.api.player.Violation
+ */
+package me.braydon.api.player;
